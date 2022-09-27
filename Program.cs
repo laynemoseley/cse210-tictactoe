27a75806275cbfa3d8a36c920c1a021b8d7d0fff
Layne Moseley <laynemoseley@gmail.com>
2022-09-27 14:00:37 -0600

    nice

--- Program.cs
+++ Program.cs
@@ -46,7 +46,12 @@
     /// <returns>True if the game is over</returns>
     static bool IsGameOver(List<string> board)
     {
-        return false;
+        if (IsWinner(board, "x") || IsWinner(board, "o") || IsTie(board)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /// <summary>Determines if the provided player has a tic tac toe.</summary>
@@ -55,6 +60,17 @@
     /// <returns></returns>
     static bool IsWinner(List<string> board, string player)
     {
+        
+        if (board[0] == player && board[1] == player && board[2] == player) {
+            return true;
+        }
+        else if (board[3] == player && board[4] == player && board[5] == player) {
+            return true;
+        }
+        else if (board[2] == player && board[4] == player && board[6] == player) {
+            return true;
+        }
+
         return false;
     }
 
@@ -71,7 +87,11 @@
     /// <returns>The next players sign (x or o)</returns>
     static string GetNextPlayer(string currentPlayer)
     {
-        return "x";
+        if (currentPlayer == "x") {
+            return "o";
+        } else {
+            return "x";
+        }
     }
 
     /// <summary>Gets the 1-based spot number associated with the user's choice.</summary>
